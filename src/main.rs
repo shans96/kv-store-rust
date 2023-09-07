883f8d68eb11d2951b7ea7c6e408785706765dc7
Shan Swanlow <40004835+shans96@users.noreply.github.com>
2023-09-07 20:38:06 +0100

    Add command-line parsing and a basic test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct CommandLineInterface {
+    /// Operation to perform on the key/value store. Can be `set`, `get`, or `rm`.
+    command: String
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = CommandLineInterface::parse();
 }
---
+++ tests/main.rs
@@ -0,0 +1,14 @@
+use assert_cmd::prelude::*; // Add methods on commands
+use predicates::prelude::*; // Used for writing assertions
+use std::process::Command; // Run programs
+
+#[test]
+fn exits_with_no_args() -> Result<(), Box<dyn std::error::Error>> {
+    let mut cmd = Command::cargo_bin("kv-store")?;
+
+    cmd.assert()
+        .failure()
+        .stderr(predicate::str::contains("the following required arguments were not provided"));
+
+    Ok(())
+}
